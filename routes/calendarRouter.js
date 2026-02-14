98dd1532834284d6bfff0c33cb4a0bc1caaa0285
hqluke <hqluke@users.noreply.github.com>
2026-02-14 14:15:21 -0700

    Added mobile responsiveness and fixed some bugs.

--- routes/calendarRouter.js
+++ routes/calendarRouter.js
@@ -9,12 +9,15 @@ const isAuthenticated = (req, res, next) => {
     res.redirect("/");
 };
 
+// Apply authentication to ALL calendar routes
+router.use(isAuthenticated);
+
 router.get("/", calendarController.getCalendar);
 router.get("/edit", calendarController.getEditExercise);
 router.post("/edit", calendarController.postEditExercise);
 router.post("/delete", calendarController.deleteExercise);
-
 router.get("/edit-run", calendarController.getEditRun);
 router.post("/edit-run", calendarController.postEditRun);
 router.post("/delete-run", calendarController.deleteRunPost);
+
 module.exports = router;
--- routes/exerciseRouter.js
+++ routes/exerciseRouter.js
@@ -1,11 +1,20 @@
 const router = require("express").Router();
 const exerciseController = require("../controllers/exerciseController");
 
+const isAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/");
+};
+
+// Apply authentication to ALL exercise routes
+router.use(isAuthenticated);
+
 router.get("/", exerciseController.getDefaultPage);
 router.get("/upper", exerciseController.getUpper);
 router.get("/lower", exerciseController.getLower);
 router.get("/run", exerciseController.getRunPage);
-
 router.get("/insertData", exerciseController.getInsertDataPage);
 router.post("/insertData", exerciseController.postInsertData);
 router.post("/insertRun", exerciseController.postInsertRun);
